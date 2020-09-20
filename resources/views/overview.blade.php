b020e3731a71782fa61816e4683cb5ccd6a67748
bilalsaleemss <bilal.s@showsuite.com>
2020-09-20 13:16:09 +0500

    Changes OverView Page

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -165,26 +165,77 @@ div.dataTables_wrapper div.dataTables_info{width: 40%;
               <tr>
                 <td>Tiger Nixon</td>
                 <td>01 January 2020</td>
-                <td><span class="dot-yel txt-yel">-</span> Team 03 <span class="dot-blue txt-blue">-</span> Team 04 <b class="txt-blue">+</b></td>
+                <td><span class="dot-yel txt-yel">-</span> Team 03 <span class="dot-blue txt-blue">-</span> Team 04 <b class="text-primary">+</b></td>
                 <td class="text-admin-primary">Online</td>
-                <td><span class="bg-admin-primary circle">-</span></td>
-                <td><a href="" data-toggle="modal" data-target="#Edit"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil penci" fill="#00C3C3" xmlns="http://www.w3.org/2000/svg">
+                <td><span class="bg-primary text-white circle">-</span></td>
+                <td><a href="" data-toggle="modal" data-target="#Edit"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil penci" fill="#8EE868" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/></svg></a></td>
-                <td><a href="" data-toggle="modal" data-target="#Del"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="#00C3C3" xmlns="http://www.w3.org/2000/svg">
+                <td><a href="" data-toggle="modal" data-target="#Del"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="#8EE868" xmlns="http://www.w3.org/2000/svg">
                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
               </svg></a></td>
               </tr>
-              
               <tr>
                 <td>Tiger Nixon</td>
                 <td>01 January 2020</td>
-                <td><span class="dot-red txt-red">-</span> Team 01 <span class="dot-green txt-green">-</span> Team 02 <b class="txt-blue">+</b></td>
+                <td><span class="dot-yel txt-yel">-</span> Team 03 <span class="dot-blue txt-blue">-</span> Team 04 <b class="text-primary">+</b></td>
                 <td class="text-admin-primary">Online</td>
-                <td><span class="bg-admin-primary circle">-</span></td>
-                <td><a href="" data-toggle="modal" data-target="#Edit"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil penci" fill="#00C3C3" xmlns="http://www.w3.org/2000/svg">
+                <td><span class="bg-primary text-white circle">-</span></td>
+                <td><a href="" data-toggle="modal" data-target="#Edit"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil penci" fill="#8EE868" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/></svg></a></td>
-                <td><a href="" data-toggle="modal" data-target="#Del"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="#00C3C3" xmlns="http://www.w3.org/2000/svg">
+                <td><a href="" data-toggle="modal" data-target="#Del"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="#8EE868" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+              </svg></a></td>
+              </tr>
+              <tr>
+                <td>Tiger Nixon</td>
+                <td>01 January 2020</td>
+                <td><span class="dot-yel txt-yel">-</span> Team 03 <span class="dot-blue txt-blue">-</span> Team 04 <b class="text-primary">+</b></td>
+                <td class="text-admin-primary">Online</td>
+                <td><span class="bg-primary text-white circle">-</span></td>
+                <td><a href="" data-toggle="modal" data-target="#Edit"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil penci" fill="#8EE868" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/></svg></a></td>
+                <td><a href="" data-toggle="modal" data-target="#Del"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="#8EE868" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+              </svg></a></td>
+              </tr>
+              <tr>
+                <td>Tiger Nixon</td>
+                <td>01 January 2020</td>
+                <td><span class="dot-yel txt-yel">-</span> Team 03 <span class="dot-blue txt-blue">-</span> Team 04 <b class="text-primary">+</b></td>
+                <td class="text-admin-primary">Online</td>
+                <td><span class="bg-primary text-white circle">-</span></td>
+                <td><a href="" data-toggle="modal" data-target="#Edit"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil penci" fill="#8EE868" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/></svg></a></td>
+                <td><a href="" data-toggle="modal" data-target="#Del"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="#8EE868" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+              </svg></a></td>
+              </tr>
+              <tr>
+                <td>Tiger Nixon</td>
+                <td>01 January 2020</td>
+                <td><span class="dot-yel txt-yel">-</span> Team 03 <span class="dot-blue txt-blue">-</span> Team 04 <b class="text-primary">+</b></td>
+                <td class="text-admin-primary">Online</td>
+                <td><span class="bg-primary text-white circle">-</span></td>
+                <td><a href="" data-toggle="modal" data-target="#Edit"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil penci" fill="#8EE868" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/></svg></a></td>
+                <td><a href="" data-toggle="modal" data-target="#Del"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="#8EE868" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+              </svg></a></td>
+              </tr>
+              <tr>
+                <td>Tiger Nixon</td>
+                <td>01 January 2020</td>
+                <td><span class="dot-yel txt-yel">-</span> Team 03 <span class="dot-blue txt-blue">-</span> Team 04 <b class="text-primary">+</b></td>
+                <td class="text-admin-primary">Online</td>
+                <td><span class="bg-primary text-white circle">-</span></td>
+                <td><a href="" data-toggle="modal" data-target="#Edit"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil penci" fill="#8EE868" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/></svg></a></td>
+                <td><a href="" data-toggle="modal" data-target="#Del"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="#8EE868" xmlns="http://www.w3.org/2000/svg">
                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
               </svg></a></td>
